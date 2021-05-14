4c27ab504266e469645f99e1552944d61869d7f1
Lucas Ransan <lucas@ransan.tk>
2021-05-14 20:32:58 +0200

    revert to default nickname after 3 hours

--- src/commands/nickname.rs
+++ src/commands/nickname.rs
@@ -1,3 +1,5 @@
+use std::time::Duration;
+
 use serenity::{
     prelude::*,
     model::prelude::*,
@@ -6,6 +8,7 @@ use serenity::{
         macros::command,
     },
 };
+use tokio::time::sleep;
 use tracing::{info, warn, error};
 
 #[command]
@@ -18,16 +21,31 @@ pub async fn nickname(ctx: &Context, msg: &Message, _args: Args) -> CommandResul
 
     if let Some(answer) = msg.channel_id.await_reply(&ctx).await {
         if let Some(guild) = msg.guild_id {
+            let new_nickname = &answer.content;
             info!(
                 "{} changed {} nickname to {}",
-                answer.author, msg.author, answer.content,
+                answer.author, msg.author, new_nickname,
             );
             if let Err(err) = guild.edit_member(
                 &ctx, msg.author.id,
-                |m| m.nickname(&answer.content)
+                |m| m.nickname(new_nickname)
             ).await {
                 error!("couldn't change {} nickname: {}", msg.author, err);
+                return Ok(());
             };
+
+            sleep(Duration::from_secs(3 * 60 * 60)).await;
+            if msg.author_nick(&ctx).await.as_deref() == Some(new_nickname) {
+                if let Err(err) = guild.edit_member(
+                    &ctx, msg.author.id,
+                    |m| m.nickname("")
+                ).await {
+                    error!("couldn't change back {} nickname: {}", msg.author, err);
+                    return Ok(());
+                }
+            } else {
+                warn!("{} nickname change, not changing back", msg.author);
+            }
         } else {
             warn!("{} asked to change nickname not in a guild", msg.author);
         }

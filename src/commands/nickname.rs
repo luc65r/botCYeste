5e0b2398026dd0b48ec9a4b966490f4f3ed3f637
Lucas Ransan <lucas@ransan.tk>
2021-05-15 11:26:04 +0200

    clean up error handling

--- src/commands/nickname.rs
+++ src/commands/nickname.rs
@@ -9,48 +9,38 @@ use serenity::{
     },
 };
 use tokio::time::sleep;
-use tracing::{info, warn, error};
+use tracing::{info, warn};
 
 #[command]
 pub async fn nickname(ctx: &Context, msg: &Message, _args: Args) -> CommandResult {
-    info!("{} asked to change nickname", msg.author);
     msg.reply(
-        &ctx,
+        ctx,
         "https://cdn.discordapp.com/attachments/750673510656901223/842843913789112340/nivkname.jpg",
     ).await?;
 
-    if let Some(answer) = msg.channel_id.await_reply(&ctx).await {
-        if let Some(guild) = msg.guild_id {
-            let new_nickname = &answer.content;
-            info!(
-                "{} changed {} nickname to {}",
-                answer.author, msg.author, new_nickname,
-            );
-            if let Err(err) = guild.edit_member(
-                &ctx, msg.author.id,
-                |m| m.nickname(new_nickname)
-            ).await {
-                error!("couldn't change {} nickname: {}", msg.author, err);
-                return Ok(());
-            };
+    let answer = msg.channel_id.await_reply(ctx).await
+        .ok_or("didn't get a reply")?;
+    let guild = msg.guild_id
+        .ok_or("not in a guild")?;
 
-            sleep(Duration::from_secs(3 * 60 * 60)).await;
-            if msg.author_nick(&ctx).await.as_deref() == Some(new_nickname) {
-                if let Err(err) = guild.edit_member(
-                    &ctx, msg.author.id,
-                    |m| m.nickname("")
-                ).await {
-                    error!("couldn't change back {} nickname: {}", msg.author, err);
-                    return Ok(());
-                }
-            } else {
-                warn!("{} nickname change, not changing back", msg.author);
-            }
-        } else {
-            warn!("{} asked to change nickname not in a guild", msg.author);
-        }
+    let new_nickname: String = answer.content
+        .trim()
+        .chars().take(32) // Discord's limit
+        .collect();
+
+    info!(
+        "{} changed {} nickname to {}",
+        answer.author, msg.author, new_nickname,
+    );
+    guild.edit_member(ctx, msg.author.id, |m| m.nickname(&new_nickname))
+        .await?;
+
+    sleep(Duration::from_secs(3 * 60 * 60)).await;
+    if msg.author_nick(ctx).await == Some(new_nickname) {
+        guild.edit_member(ctx, msg.author.id, |m| m.nickname(""))
+            .await?;
     } else {
-        error!("{} didn't recieve an answer to change his nickname", msg.author);
+        warn!("{} nickname changed, not changing back", msg.author);
     }
 
     Ok(())
